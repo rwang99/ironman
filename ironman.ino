df22fa7b935dde8463e315abb389d984c45d1c7b
Richard Wang <rkwang@umd.edu>
2018-02-25 16:53:30 -0500

    Added on/off switch capability

--- ironman.ino
+++ ironman.ino
@@ -6,6 +6,8 @@
 #define NUM_LIGHTS 10
 #define BRIGHTNESS 40
 
+#define SWITCH 21
+
 void setup() {
   
   init_neopixel_blit();
@@ -17,12 +19,19 @@ void loop() {
   int i;
   Pixels pix = {{0}};
 
-  for (i = 0; i<NUM_LIGHTS; i++){
-    pix[i][PIX_RED] = BRIGHTNESS;
-    pix[i][PIX_BLU] = BRIGHTNESS;
-    pix[i][PIX_GRN] = BRIGHTNESS;
-  } 
-               
+  if (digitalRead(SWITCH)){
+    for (i = 0; i<NUM_LIGHTS; i++){
+      pix[i][PIX_RED] = BRIGHTNESS;
+      pix[i][PIX_BLU] = BRIGHTNESS;
+      pix[i][PIX_GRN] = BRIGHTNESS;
+    } 
+  } else {
+    for (i = 0; i<NUM_LIGHTS; i++){
+      pix[i][PIX_RED] = 0;
+      pix[i][PIX_BLU] = 0;
+      pix[i][PIX_GRN] = 0;
+    } 
+  }
   neopixel_blit(pix); 
   delay(1000);
 }
