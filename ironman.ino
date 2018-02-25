71608d8c246ba78394ec96cb49ac6c70709af81c
Richard Wang <rkwang@umd.edu>
2018-02-25 16:46:44 -0500

    Basic lighting working

--- ironman.ino
+++ ironman.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <stdio.h>
+#include <serial_interface.h>
+#include <neopixel_blit.h>
 
+#define NUM_LIGHTS 10
+#define BRIGHTNESS 40
+
+void setup() {
+  
+  init_neopixel_blit();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+Pixels all_offs = {{0}};
 
+void loop() {
+  int i;
+  Pixels pix = {{0}};
+
+  for (i = 0; i<NUM_LIGHTS; i++){
+    pix[i][PIX_RED] = BRIGHTNESS;
+    pix[i][PIX_BLU] = BRIGHTNESS;
+    pix[i][PIX_GRN] = BRIGHTNESS;
+  } 
+               
+  neopixel_blit(pix); 
+  delay(1000);
 }
